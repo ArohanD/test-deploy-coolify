c1982a1425beca63b8f9a4c4dc7e68697c05c9cd
Arohan Dutt <arohandutt@gmail.com>
2025-03-04 23:53:07 -0500

    try moving assets

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,11 @@
+import { assets } from '$app/paths';
 import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
 		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
@@ -10,7 +13,7 @@ export default {
 			strict: true
 		}),
 		paths: {
-			base: '/testkit'
+			assets: "https://apps.arohan.dev/testkit/"
 		}
 	}
 };
